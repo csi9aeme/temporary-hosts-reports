1629ffc1ee47d7b54ffffce50d9f6b13823a91a8
csi9aeme <csi9aeme@gmail.com>
2023-08-15 12:21:50 +0200

    init

---
+++ src/main/java/hostsreport/hhr/HostReportsApplication.java
@@ -0,0 +1,13 @@
+package hostsreport.hhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hostsreport/hhr/HostReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package hostsreport.hhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
